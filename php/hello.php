6648dbacb299e2c477773d28a3ad0fc235335bc7
Jfie <jfie@ijjnj.in>
2018-10-08 20:07:53 +0300

    New PHP-v1

---
+++ php/hello.php
@@ -0,0 +1,10 @@
+<! DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Начинаем работу с PHP</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ php/string.php
@@ -0,0 +1,17 @@
+<! DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Начинаем работу с PHP</title>
+</head>
+<body>
+<?php
+$phrase = 'Правда редко бывает чистой';
+$author = 'Оскар Уайльд';
+echo $phrase;
+echo "<p>Часто говорят, что <q>$phrase</q></p>";
+$phrase = $phrase. ' и никогда не бывает простой';
+echo "<p><q>$phrase</q><cite>$author</cite></p>";
+?>
+</body>
+</html>
