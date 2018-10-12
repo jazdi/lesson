ac2aaec269698e4aba8466f244e644020ed0028c
Jfie <jfie@ijjnj.in>
2018-10-12 16:42:29 +0300

    add file Matrix

---
+++ php/matrix.php
@@ -0,0 +1,22 @@
+<! DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Начинаем работу с PHP</title>
+</head>
+<body>
+<?php
+$letters = array('А', 'Б', 'В');
+$numbers = array(1, 2, 3);
+$matrix = ['Буква' => $letters, 'Число' => $numbers];
+echo "<p>Начало: {$matrix['Буква'][0]} </p>";
+foreach ($matrix as $array => $list) {
+	echo '<ul>';
+	foreach($list as $key => $value) {
+		echo "<li>$array [$key] = $value ";
+	}
+	echo '</ul>';
+}
+?>
+</body>
+</html>
