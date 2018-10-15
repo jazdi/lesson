4626794f09ca5ec1055c27eca2d2fc74ced8d88f
Dmitriy <jazdi@yandex.ru>
2018-10-15 15:55:20 +0300

    Добавление файла с проверкой условия

---
+++ php/condition.php
@@ -0,0 +1,20 @@
+<! DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Условные операторы в PHP</title>
+</head>
+<body>
+<?php
+$a = NULL;
+$b = 8;
+$c = 'PHP - это весело!';
+$verb = ($b == 1)? 'угадали': 'не угадали';
+echo "Вы $verb! Вы назвали число $b. <hr>";
+$parity = ($b% 2!=0)? 'нечетное': 'четное';
+echo "$b - $parity число<hr>";
+$result = $a?? $b?? $c; echo "abc: $result <br>";
+$result = $c?? $b?? $a; echo "cba: $result <br>";
+?>
+</body>
+</html>
